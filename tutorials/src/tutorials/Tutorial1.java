8bc9b8375838f25cf4368700f33e3a11c8c06034
Spindelmyran <tonyztonyz@hotmail.com>
2014-10-12 19:39:20 +0200

    commit

--- tutorials/src/tutorials/Tutorial1.java
+++ tutorials/src/tutorials/Tutorial1.java
@@ -11,9 +11,14 @@ public class Tutorial1 {
        int myNumber=88;
        short myShort = 3434;
        long myLong = 734322948;
+       
+       double myDouble = 7.3243;
+       float myFloat = 324.3f;
         System.out.println(myNumber);
         System.out.println(myShort);
         System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
     }
     
 }
