24893fb950784d6fafcb4c92cf140e5ac958f0a5
Spindelmyran <tonyztonyz@hotmail.com>
2014-10-12 21:25:56 +0200

    com

---
+++ tutorials/src/tutorials/TutorialStrings.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutorials;
+
+/**
+ *
+ * @author tonyandersson
+ */
+public class TutorialStrings {
+    public static void main(String[] args) {
+        int myInt = 7;
+        String text = "Hello";
+        String blank = " ";
+        String name = "Bob";
+        String greeting = text + blank + name;
+        System.out.println(greeting);
+        System.out.println("Hello"+" "+"Bob");
+        System.out.println("MY integer is: "+myInt);
+    }
+}
