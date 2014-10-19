f7d66fb67580d5cee7738c5014eecb622b0975a2
Spindelmyran <tonyztonyz@hotmail.com>
2014-10-19 20:41:02 +0200

    getter

--- tutorials/src/ClassesAndObjects.java
+++ tutorials/src/ClassesAndObjects.java
@@ -7,6 +7,11 @@ class Person {
     // classes can contain
     // 1. Data
     // 2. Subroutines (methods)
+    void speak(){
+        for(int i=0;i<3;i++){
+        System.out.println("My name is: "+name+" and i am "+age+" years old ");
+        }
+    }
 };
 
 public class ClassesAndObjects {
@@ -15,10 +20,12 @@ public class ClassesAndObjects {
         Person person1 = new Person();
         person1.name = "Joe Bloggs";
         person1.age = 37;
+        person1.speak();
 
         Person person2 = new Person();
         person2.name = "Sarah Smith";
         person2.age = 20;
+        person2.speak();
         System.out.println(person1.name);
         System.out.println(person2.age);
     }
