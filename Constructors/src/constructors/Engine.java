5ff299493da6e44b35f956588e8a1c37a7224945
Spindelmyran <tonyztonyz@hotmail.com>
2014-10-26 19:05:19 +0100

    new

--- Constructors/src/constructors/Engine.java
+++ Constructors/src/constructors/Engine.java
@@ -1,36 +1,26 @@
 package constructors;
 
-class Machine {
+
+public class Engine {
 
     private String name;
     private int code;
 
-    public Machine() {
+    public Engine() {
         this("Arnie", 0);
         System.out.println("Constructor running!");
     }
 
-    public Machine(String name) {
+    public Engine(String name) {
         this(name, 0);
 
         System.out.println("Second constructor running");
         this.name =name;
     }
 
-    public Machine(String name, int code) {
+    public Engine(String name, int code) {
         System.out.println("Third constructor running");
         this.name = name;
         this.code = code;
-    }
-
-}
-
-public class Constructors {
-
-    public static void main(String[] args) {
-        Engine Engine1 = new Engine();
-        /*Machine machine2 = new Machine("Bertie");
-        Machine machine3 = new Machine("Chalky",7);*/
-    }
-
+    }    
 }
---
+++ com.cakes/src/com/cakes/MealBuilder.java
@@ -0,0 +1,12 @@
+
+package com.cakes;
+
+public interface MealBuilder {
+	public void buildDrink();
+
+	public void buildMainCourse();
+
+	public void buildSide();
+
+	public Meal getMeal();
+}
