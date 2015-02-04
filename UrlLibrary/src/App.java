7e756bae379d54b548cbd0951fa2008ab2f07735
Tony Andersson <spindelmyran@gmail.com>
2015-02-04 07:45:05 +0100

    new

---
+++ UrlLibrary/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class App {
+
+    public static void main(String[] args) {
+
+       UrlLibrary urlLibrary = new UrlLibrary();
+       
+       for(String html: urlLibrary){
+           System.out.println(html.length());
+           System.out.println(html);
+       }
+    }
+
+}
